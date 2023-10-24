1b3cae2ebb90cfd4583b858523b1d07f088e9614
Kevin7744 <arapbiisubmissions@gmail.com>
2023-10-24 00:56:40 -0700

    Safe print integer list

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result =  a / b
-    except (ZeroDivisionError, TypeError):
-        result = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        if result is not None:
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: None")
-    if result is not None:
-        print("{:d} / {:d} = {}".format(a, b, result))
-    else:
-        print("{:d} / {:d} = {}".format(a, b, result))
-
+        print("Inside result: {}".format(div))
+    return (div)
