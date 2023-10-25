cb9cd3b756c95c405b9fc8dc149aa92fcaf54b85
Kevin7744 <arapbiisubmissions@gmail.com>
2023-10-25 00:20:18 -0700

    Pycode corretion

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ class definition Square """
+
+
 class Square:
-    """ 
-	Size private attribute initialized in self constructor
-    """"
+    """ Size private attribute initialized in self constructor """"
     def __init__(self, size):
         """ Create a new instance of square which is of 1 side
         size: the size of the square
-	"""
+        """
         self.__size = size
-
-
