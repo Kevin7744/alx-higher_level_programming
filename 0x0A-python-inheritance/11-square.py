82b29d0b7dc0bf8cfe2d6eb797930d6b584eb2ea
Kevin7744 <arapbiisubmissions@gmail.com>
2023-11-06 06:55:20 -0800

    11Square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Class Rectangle that inherits from BaseGeomtry
+Contains the class BaseGeometry and subclass Rectangle
 """
 
 
@@ -19,18 +19,35 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """  representaion of a rectangle """
+    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """ Instatiation of rectangle """
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """ returns are of the rectangle """
+        """returns the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """ Informal string representation of the rectangle """
+        """informal string representation of the rectangle"""
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size):
+        """instantiation of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
