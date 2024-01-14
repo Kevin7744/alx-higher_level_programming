39ab6d9d91ed790f32ca5fcf0aae0576deb55260
root <root@Toor.>
2024-01-14 12:13:02 +0300

    List all with name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT *\
+    cur.execute("SELECT * \
     FROM states \
-    WHERE CONVERT(`name` USING Latin1)
+    WHERE CONVERT(`name` USING Latin1) \
     COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
